9c51d00d10a6d828c693405ced75ef31e4260444
miguelrosasvillalobos <miguel.rosas2001@alumnos.ubiobio.cl>
2024-07-29 18:49:42 -0400

    se suben scripts

---
+++ start_simulation.sh
@@ -0,0 +1,43 @@
+#Miguel Rosas
+
+# Verifica si se proporciona la cantidad_simulaciones como argumento
+if [ $# -eq 0 ]; then
+	echo "Uso: $0 cantidad_simulaciones"
+	exit 1
+fi
+
+# Obtiene la cantidad_simulaciones desde el primer argumento
+cantidad_simulaciones=$1
+
+# Leer valores desde el archivo parametros.txt
+rd=$(grep -oP 'rd\s*=\s*\K[\d.+-]+' parametros.txt)
+a=$(grep -oP 'a\s*=\s*\K[\d.+-]+' parametros.txt)
+rp=$(grep -oP 'rp\s*=\s*\K[\d.+-]+' parametros.txt)
+np=$(grep -oP 'np\s*=\s*\K[\d.+-]+' parametros.txt)
+
+# Bucle para crear y mover carpetas, editar y genrar mallado
+for ((i = 1; i <= $cantidad_simulaciones; i++)); do
+	# Genera el nombre de la carpeta
+	carpeta_caso_i="Case_$i"
+
+	# Crea la carpeta del caso
+	mkdir "$carpeta_caso_i"
+
+	# Copia carpetas del caso dentro de las carpetasgeneradas
+	cp -r "Case_0/geometry_script/" "$carpeta_caso_i/"
+	cd "$carpeta_caso_i/"
+
+	# Reemplazar valores en sus respectivos archivos
+	sed -i "s/\$npp/$np/g" ./geometry_script/generator_point_process.py
+	sed -i "s/\$rpp/$rp/g" ./geometry_script/generator_point_process.py
+	sed -i "s/\$rdd/$rd/g" ./geometry_script/generator_point_process.py
+
+	cd "./geometry_script/"
+	#Generar mallado gmsh
+	python3 generator_point_process.py
+	mv conteo_puntos.txt ../
+	mv puntos.csv ../
+	cd ../..
+done
+
+echo "Proceso completado."
